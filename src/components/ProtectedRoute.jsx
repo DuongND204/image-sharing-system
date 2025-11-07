fcd2ea62393da6dbe145f4e446aedbf74acc2e13
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2025-11-07 11:39:52 +0700

    auth feature done

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
-import { Navigate } from 'react-router-dom';
-import { useAuthStore } from '../store/useAuthStore';
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
 
+// Protected Route
 export default function ProtectedRoute({ children }) {
   const { authUser } = useAuthStore();
 
   if (!authUser) {
-    return <Navigate to='/login' replace />;
+    return <Navigate to="/login" replace />;
   }
 
   return children;
